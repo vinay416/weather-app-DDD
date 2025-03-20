f2535ea36068ae050aaecc0b7b071f47ad4d319c
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-03-20 10:14:07 +0530

    initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_app_ddd
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/weather_app_ddd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weather_app_ddd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
